0db5e9b9b3f7b385d8cad9a4154a2b92696ae936
Mark Paulukevich <paulukevics@gmail.com>
2023-07-21 16:10:01 +0300

    Improved stripe implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,62 +1,77 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
 const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
+const AppError = require('./utils/appError');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
-  // 1) Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
-  // console.log(tour);
-  const transformedItems = [
-    {
-      quantity: 1,
-      price_data: {
-        currency: 'usd',
-        unit_amount: tour.price * 100,
-        product_data: {
-          name: `${tour.name} Tour`,
-          description: tour.description, //description here
-          images: [
-            // `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
-            `https://www.natours.dev/img/tours/${tour.imageCover}`
-          ] //only accepts live images (images hosted on the internet),
-        }
-      }
-    }
-  ];
 
-  // 2) Create checkout session
+  if (!tour) return next(new AppError('Tour not found!', 404));
+
+  // create a checkout session
   const session = await stripe.checkout.sessions.create({
+    mode: 'payment',
     payment_method_types: ['card'],
-    // success_url: `${req.protocol}://${req.get('host')}/`, //user will be redirected to this url when payment is successful. home page
-    // cancel_url: `${req.protocol}://${req.get('host')}/${tour.slug}`, //user will be redirected to this url when payment has an issue. tour page (previous page)
-    success_url: `${req.protocol}://${req.get('host')}/?tour=${
-      req.params.tourId
-    }&user=${req.user.id}&price=${tour.price}`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
-    client_reference_id: req.params.tourId, //this field allows us to pass in some data about this session that we are currently creating.
-    line_items: transformedItems,
-    mode: 'payment'
+    client_reference_id: req.params.tourId,
+    line_items: [
+      {
+        description: `${tour.summary}`,
+        price_data: {
+          unit_amount: tour.price * 100,
+          currency: 'usd',
+          product_data: {
+            name: tour.name,
+            description: `${tour.summary}`,
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${
+                tour.imageCover
+              }`
+            ]
+          }
+        },
+        quantity: 1
+      }
+    ]
   });
 
-  // 3) Create session as response
+  // res.redirect(303, session.url); // Ignore this, only for front-end implementation via form action
   res.status(200).json({
     status: 'success',
     session
   });
 });
 
-exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-  // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
-  const { tour, user, price } = req.query;
-
-  if (!tour && !user && !price) return next();
+const createBookingCheckout = async session => {
+  const tour = session.client_reference_id;
+  const user = (await User.findOne({ email: session.customer_email }))._id;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
+};
 
-  res.redirect(req.originalUrl.split('?')[0]);
-});
+exports.webhookCheckout = (req, res, next) => {
+  const sig = req.headers['stripe-signature'];
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(
+      req.body,
+      sig,
+      process.env.WEBHOOK_SECRET
+    );
+  } catch (err) {
+    return res.status(400).send(`Webhook Error ${err.message}`);
+  }
+
+  if (event.type === 'checkout.session.completed')
+    createBookingCheckout(event.data.object);
+
+  res.status(200).json({ received: true });
+};
 
 exports.createBooking = factory.createOne(Booking);
 exports.getBooking = factory.getOne(Booking);
